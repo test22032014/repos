3b294cab1aa2f41d71a5992730b295ba377e8238
test22032014 <m.hffmnn@web.de>
2014-03-28 23:41:35 +0100

    foo bar
    schon toll

---
+++ lib/test22032014/Foo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace test22032014;
+
+class Foo
+{
+	public function toString()
+	{
+		return 'foo';
+	}
+}
---
+++ lib/test22032014/bar/Bar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace test22032014\bar;
+
+class Bar
+{
+	public function toString()
+	{
+		return 'bar';
+	}
+}
